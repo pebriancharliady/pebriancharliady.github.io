eef952fc554bc29915f609d63ce22a0a5eccc836
pebriancharliady <rianfebrian302@gmail.com>
2020-08-08 16:54:14 +0700

    init 3

--- js/scrollTrigger.js
+++ js/scrollTrigger.js
@@ -1,6 +1,17 @@
-let st = ScrollTrigger.create({
-    trigger: ".trigger",
-    pin: ".pin",
-    start: "top center",
-    end: "+=500"
-  });
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from("#me", {
+  duration:1.75, y:"-100px", ease:'sine',opacity:0
+});
+
+gsap.from("#circle-1", {
+  duration:2.25, y:"100px", ease:"sine",opacity:0
+});
+
+gsap.from("#circle-2", {
+  duration:2, y:"-100px", ease:"sine",opacity:0
+});
+
+gsap.from("#aboutme video", {
+  duration:1.75, x:"100px", ease:"sine",opacity:0, scrollTrigger: '#aboutme video'
+});
